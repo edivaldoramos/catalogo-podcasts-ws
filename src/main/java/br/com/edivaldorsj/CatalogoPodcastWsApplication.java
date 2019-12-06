514ce8b0c2d9983a00dff6b243a25b41678988df
Edivaldo <edivaldoramossj@gmail.com>
2019-12-06 09:31:52 -0300

    Criando projeto

---
+++ src/main/java/br/com/edivaldorsj/CatalogoPodcastWsApplication.java
@@ -0,0 +1,13 @@
+package br.com.edivaldorsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoPodcastWsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CatalogoPodcastWsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/edivaldorsj/CatalogoPodcastWsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edivaldorsj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoPodcastWsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
